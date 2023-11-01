1c7e7b0370ccf7a417abe92b21658e5cfd235c6b
jojoyal <josepjoyal@gmail.com>
2023-11-01 09:27:54 +0530

    feat: authorization in progress Fixes #2

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { MaxWidthWrapper } from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
+
+const NavBar = () => {
+	return (
+		<nav className="sticky h-14 inset-x-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all ">
+			<MaxWidthWrapper>
+				<div className="flex h-14 items-center justify-between border-b border-zinc-200">
+					<Link href={"/"} className="flex z-40 font-semibold text-xl">
+						PDF-GPT {/*<span className="text-sm text-green-700 pt-1 ps-2"> by gptsan</span> */}
+					</Link>
+					{/* TODO: Mobile navbar */}
+					<div className="hidden items-center space-x-4 sm:flex">
+						<>
+							<Link href={"/pricing"} className={buttonVariants({ variant: "ghost", size: "sm" })}>
+								Pricing
+							</Link>
+							<LoginLink className={buttonVariants({ variant: "ghost", size: "sm" })}>
+								Sign in
+							</LoginLink>
+							<RegisterLink className={buttonVariants({ variant: "default", size: "sm" })}>
+								Get started <ArrowRight className="ml-1.5 h-4 w-4"/>
+							</RegisterLink>
+						</>
+					</div>
+				</div>
+			</MaxWidthWrapper>
+		</nav>
+	);
+};
+
+export { NavBar };
